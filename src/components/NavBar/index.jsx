57f0036684babb7be39401fba4753277e4512d4b
thiagotnaraujo <thago@crowdability.com>
2024-11-26 21:49:25 -0300

    NavBar componentizada

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -4,6 +4,8 @@ import { LogIn } from 'lucide-react';
 import { useAuth } from '@/context/AuthContext';
 import { Link, useLocation } from 'react-router-dom';
 import { useNavigation } from '@/context/NavigationContext';
+import Header from '@/components/Header';
+import MainContent from '@/components/MainContent';
 
 const Navbar = ({ children }) => {
   const location = useLocation();
@@ -152,14 +154,13 @@ const Navbar = ({ children }) => {
         </DisclosurePanel>
       </Disclosure>
 
-      <header className="bg-white shadow">
-        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold tracking-tight text-gray-900">{currentNavigationItem ? currentNavigationItem.name : ''}</h1>
-        </div>
-      </header>
-      <main>
-        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">{children}</div>
-      </main>
+      <Header>
+        {currentNavigationItem ? currentNavigationItem.name : ''}
+      </Header>
+      
+      <MainContent>
+        { children }
+      </MainContent>
     </div>
   );
 };
